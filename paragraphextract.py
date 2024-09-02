d3ca1016790e9c873ab41756f45a9e7224493668
DanielGroelle <DanielGroelle@protonmail.com>
2024-09-02 00:13:28 -0400

    Implement filtering, length matching, and writing to csv output

--- paragraphextract.py
+++ paragraphextract.py
@@ -1,7 +1,11 @@
 import csv
+import re
 from os.path import exists
 from os import makedirs
 from os import scandir
+
+LINE_CHARACTER_MIN = 20
+LINE_CHARACTER_MAX = 280
 
 if not exists("./texts"):
   makedirs("./texts")
@@ -9,17 +13,34 @@ if not exists("./texts"):
 texts = scandir("./texts")
 for text in texts:
   if text.is_file():
-    print(text.name)
-    title = text.name.split(" - ")[0]
-    author = text.name.split(" - ")[1].split(".txt")[0]
-    print(title)
-    print(author)
-    
-    file = open("./texts/" + text.name, encoding="utf-8")
-    print(file.readlines()[0:2])
+    author = text.name.split(" - ")[0]
+    title = text.name.split(" - ")[1].split(".txt")[0]
+    print(author + " - " + title)
 
     outputfile = open("./texts/" + author + " - " + title + ".csv", "w", encoding="utf-8", newline='')
     writer = csv.writer(outputfile)
+
+    file = open("./texts/" + text.name, encoding="utf-8")
+
+    lines = file.readlines()
+    for line in lines:
+      print(line)
+      # filter out newlines, replace emdashes with regular dashes, and replace alternate quotations with straight digital quotations
+      line = line.replace("\n", "").replace("—", "-").replace("‘", "'").replace("’", "'").replace("“", '"').replace("”", '"').replace("«", '"').replace("»", '"')
+      print(line)
+      # make sure line is longer than the minimum, and starts on a valid character
+      if re.compile(r'^[A-Z\d]+').match(line) != None:
+        if len(line) >= LINE_CHARACTER_MIN and line[0] != " ":
+          if len(line) >= LINE_CHARACTER_MAX:
+            splitlines = re.split("([?!.])", line)
+            for splitline in splitlines:
+              if len(splitline) >= LINE_CHARACTER_MIN and len(splitline) <= LINE_CHARACTER_MAX:
+                writer.writerow([splitline])
+                # print(splitline)
+          else:
+            writer.writerow([line])
+
+    outputfile.close()
     file.close()
 
 texts.close()
