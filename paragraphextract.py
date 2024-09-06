375829e6321ce145f66785e32a393f0193f143c1
DanielGroelle <DanielGroelle@protonmail.com>
2024-09-05 23:13:33 -0400

    Fix issue with splitting before quote ends, and rewrite to be cleaner looking

--- paragraphextract.py
+++ paragraphextract.py
@@ -7,42 +7,68 @@ from os import scandir
 LINE_CHARACTER_MIN = 70
 LINE_CHARACTER_MAX = 360
 
-if not exists("./texts/processed"):
-  makedirs("./texts/processed")
+def read_file(text):
+  author = text.name.split(" - ")[0]
+  title = text.name.split(" - ")[1].split(".txt")[0]
+  print(author + " - " + title)
 
-texts = scandir("./texts")
-for text in texts:
-  if text.is_file():
-    author = text.name.split(" - ")[0]
-    title = text.name.split(" - ")[1].split(".txt")[0]
-    print(author + " - " + title)
+  output_file = open("./texts/processed/" + author + " - " + title + ".csv", "w", encoding="utf-8", newline='')
+  writer = csv.writer(output_file)
+  writer.writerow([author, title])
 
-    outputfile = open("./texts/processed/" + author + " - " + title + ".csv", "w", encoding="utf-8", newline='')
-    writer = csv.writer(outputfile)
-    writer.writerow([author, title])
+  file = open("./texts/" + text.name, encoding="utf-8")
+  lines = file.readlines()
+  for line in lines:
+    # filter out newlines, replace emdashes with regular dashes, and replace alternate quotations with straight digital quotations
+    line = line.replace("\n", "").replace("—", "-").replace("–", "-").replace("‘", "'").replace("’", "'").replace("“", '"').replace("”", '"').replace("«", '"').replace("»", '"').replace("„", '"')
+    line = line.strip()
 
-    file = open("./texts/" + text.name, encoding="utf-8")
+    # make sure line starts on a valid uppercase character
+    if re.compile(r'^\P{Ll}*').match(line) != None:
+      # make sure line is longer than minimum
+      if len(line) >= LINE_CHARACTER_MIN:
+        if len(line) >= LINE_CHARACTER_MAX:
+          long_line = split_long_lines(line)
+          if (len(long_line)):
+            writer.writerow([long_line])
+        else:
+          writer.writerow([line])
 
-    lines = file.readlines()
-    for line in lines:
-      # filter out newlines, replace emdashes with regular dashes, and replace alternate quotations with straight digital quotations
-      line = line.replace("\n", "").replace("—", "-").replace("–", "-").replace("‘", "'").replace("’", "'").replace("“", '"').replace("”", '"').replace("«", '"').replace("»", '"')
-      line = line.strip()
+  output_file.close()
+  file.close()
 
-      # make sure line is longer than the minimum, and starts on a valid uppercase character
-      if re.compile(r'^\P{Ll}*').match(line) != None:
-        if len(line) >= LINE_CHARACTER_MIN and line[0] != " ":
-          if len(line) >= LINE_CHARACTER_MAX:
-            # split on punctuation while preserving the punctuation
-            splitlines = re.split("([?!.])", line)
-            for splitline in splitlines:
-              if len(splitline) >= LINE_CHARACTER_MIN and len(splitline) <= LINE_CHARACTER_MAX:
-                splitline = splitline.strip()
-                writer.writerow([splitline])
-          else:
-            writer.writerow([line])
+def split_long_lines(line: str):
+  # split on punctuation while preserving the punctuation
+  punctuation_separators = re.compile("([\.!?]\"?)")
+  split_lines = re.split(punctuation_separators, line)
 
-    outputfile.close()
-    file.close()
+  # find punctuation in split_lines, and concat with the previous sentence
+  new_split_lines = []
+  for concat_line in range(0, len(split_lines)):
+    if (punctuation_separators.match(split_lines[concat_line])):
+      new_split_lines[len(new_split_lines)-1] = new_split_lines[len(new_split_lines)-1] + split_lines[concat_line]
+    else:
+      new_split_lines.append(split_lines[concat_line])
+  split_lines: list[str] = new_split_lines
+  
+  # accumulate on line_to_write until max length is reached and return result
+  line_to_write = ""
+  for split_line in split_lines:
+    new_length = len(line_to_write + split_line)
+    if new_length >= LINE_CHARACTER_MIN and new_length <= LINE_CHARACTER_MAX:
+      line_to_write += split_line
+    else:
+      return line_to_write
 
-texts.close()
+def main():
+  if not exists("./texts/processed"):
+    makedirs("./texts/processed")
+
+  texts = scandir("./texts")
+  for text in texts:
+    if text.is_file():
+      read_file(text)
+
+  texts.close()
+
+main()
